d925bc78a3fe6433f3d1535c6bbb63efa6870446
Thomas Poulain <61588557+thomaspoulain0611@users.noreply.github.com>
2020-04-27 21:32:01 +0200

    reglages

--- piscine/main.cpp
+++ piscine/main.cpp
@@ -10,13 +10,11 @@ int main()
     a.poidsarete();
     std::cout<<a<<std::endl;
     a.dessiner(svgout);
-<<<<<<< HEAD
-=======
 
 
 
 
 
->>>>>>> 8d26d411f18301f909c0d7e0c8c3cfb2484b3588
+
     return 0;
 }
