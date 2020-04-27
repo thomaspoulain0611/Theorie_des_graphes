db147f366f1f30ce841d3a9bb09261048b169dc6
Thomas Poulain <61588557+thomaspoulain0611@users.noreply.github.com>
2020-04-27 11:32:02 +0200

    Chargement

--- piscine/include/Graphe.h
+++ piscine/include/Graphe.h
@@ -1,31 +1,17 @@
 #ifndef GRAPHE_H
 #define GRAPHE_H
-#include <iostream>
-#include <vector>
 
-#include <Sommet.h>
-#include <Arete.h>
 
 class Graphe
 {
     public:
-        Graphe();
-        ~Graphe();
+        Graphe(std::string nomFichier);
+        virtual ~Graphe();
 
-        int getOrdre();
-        int getTaille();
-        int getOrient();
 
-        void setOrdre();
-        void setTaille();
-        void setOrient();
 
     private:
-        int m_ordre;
-        int m_taille;
-        int m_orient;
-        std::vector<Sommet*> m_vect_som;
-        std::vector<Arete*> m_vect_aret;
+
 };
 
 #endif // GRAPHE_H
--- piscine/src/Graphe.cpp
+++ piscine/src/Graphe.cpp
@@ -1,41 +1,58 @@
 #include "Graphe.h"
 
-Graphe::Graphe()
+
+
+    Graphe::Graphe(std::string nomFichier)
 {
-    //ctor
+    std::ifstream ifs{nomFichier};//
+    if(!ifs)// on verifie que le fo
+        throw std::runtime_error("Impossible d'ouvrir en lecture" + nomFichier );
+        int orientation;
+        ifs>>orientation;
+    int ordre;
+    ifs>>ordre;
+    if (ifs.fail() )
+        throw std::runtime_error("Erreur de lecture d'ordre");
+
+
+        for (size_t i=0;i<ordre;++i)
+        {
+            m_sommets.push_back(new Sommet(ifs))
+        }
+        int taille;
+        ifs>>taille;
+        if (ifs.fail() )
+        throw std::runtime_error("Erreur de lecture d'ordre");
+        for (size_t i=0;i<taille;++i)
+        {
+            m_aretes.push_back(new Arete(ifs))
+        }
+
+
+   /* int taille;
+    ifs>>taille;
+    if ( ifs.fail() )
+        throw std::runtime_error("Erreur de lecture de taille");
+    for (int i=0; i<ordre; ++i)
+    {
+        m_sommets.push_back(new Sommet{i+1});// vu que ca part de 0 on lui attribue la valeur i+1
+    }
+    for (int i=0; i<taille; ++i)
+    {
+        m_aretes.push_back(new Arete(ifs));// lire le prochain truc que le flux va lire
+    }*/
+}
+
+Graphe::~Graphe()
+{
+    for (auto s : m_sommets)
+        delete s;
+    for (auto s : m_aretes)
+        delete s;
+}
 }
 
 Graphe::~Graphe()
 {
     //dtor
-}
-
-Graphe::getOrdre()
-{
-    return m_ordre;
-}
-
-Graphe::getOrient()
-{
-    return m_orient;
-}
-
-Graphe::getTaille()
-{
-    return m_taille;
-}
-
-Graphe::setOrdre()
-{
-
-}
-
-Graphe::setOrient()
-{
-
-}
-
-Graphe::setTaille()
-{
-
 }
